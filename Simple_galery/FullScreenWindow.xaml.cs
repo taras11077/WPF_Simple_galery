6ea9867cffddb5fd4a296c952b2ce33ba7476f40
Taras Shevchuk <stg11077@gmail.com>
2023-05-05 13:13:27 +0300

    add drag-n-drop, scroll on MouseWeel, MouseEnter-MouseLeave

---
+++ Simple_galery/FullScreenWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Simple_galery
+{
+    /// <summary>
+    /// Логика взаимодействия для FullScreenWindow.xaml
+    /// </summary>
+    public partial class FullScreenWindow : Window
+    {
+        private List<string> list = new List<string>();
+        public int Id { get; set; }
+        public FullScreenWindow(List<string> list, int id)
+        {
+            InitializeComponent();
+
+            this.list = list;
+            Id = id;
+
+            ConfigWindow();
+
+            imgMain.Source = new BitmapImage(new Uri(list[id]));
+        }
+
+        private void ConfigWindow()
+        {
+            WindowStyle = WindowStyle.None;
+            WindowState = WindowState.Maximized;
+            ResizeMode = ResizeMode.NoResize;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Mouse.OverrideCursor = Cursors.None;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    Mouse.OverrideCursor = Cursors.Arrow;
+                    Close();
+                    break;
+                case Key.Right:
+                    if (Id == list.Count - 1)
+                        return;
+                    imgMain.Source = new BitmapImage(new Uri(list[++Id]));
+                    break;
+                case Key.Left:
+                    if (Id == 0)
+                        return;
+                    imgMain.Source = new BitmapImage(new Uri(list[--Id]));
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Simple_galery/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using FileProcessor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_galery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Border? activeBorder = null;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private List<string> list = new List<string>();
+        private Finder finder;
+
+        //private Image imagePreview;
+
+        public Image ImagePreview { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            fbd = new System.Windows.Forms.FolderBrowserDialog();
+            finder = new Finder();
+            ImagePreview = new Image();
+
+            string[] filePatterns =
+            {
+                "*.JPG",
+                "*.jpeg",
+                "*.jpg",
+                "*.png",
+                "*.gif",
+            };
+
+            finder.FileMasks = filePatterns;
+        }
+
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            if (fbd.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+            {
+                finder.FindFiles(fbd.SelectedPath);
+
+                finder.Container.Files.ForEach(f =>
+                {
+                    list.Add(f.FullName);
+                });
+            }
+            UpdateStack();
+        }
+        private void UpdateStack()
+        {
+            list.ForEach(path =>
+            {
+                System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                image.Source = new BitmapImage(new Uri(path));
+                image.MouseWheel += scrollViewer_MouseWheel;
+
+                Border border = new Border()
+                {
+                    Child = image,
+
+                    MinWidth = 150,
+                    BorderThickness = new Thickness(3, 3, 3, 3),
+                    Margin = new Thickness(10, 15, 10, 15),
+                };
+
+                border.MouseDown += Border_MouseDown;
+                border.MouseEnter += Border_MouseEnter;
+                border.MouseLeave += Border_MouseLeave;
+                border.MouseWheel += scrollViewer_MouseWheel;
+
+                stack.Children.Add(border);
+            });
+        }
+        private void SelectImage(object sender)
+        {
+            Border? border = sender as Border;
+            if (border != null)
+            {
+                if (activeBorder != null)
+                    activeBorder.BorderBrush = new SolidColorBrush(Colors.Transparent);
+
+                activeBorder = border;
+                border.BorderBrush = new SolidColorBrush(Colors.DarkOliveGreen);
+
+                if (border.Child is Image image)
+                    imgPreview.Source = image.Source;
+            }
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                SelectImage(sender);
+            }
+        }
+        private void menuFullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeBorder == null)
+                return;
+
+            FullScreenWindow fsWin = new FullScreenWindow(list, stack.Children.IndexOf(activeBorder));
+            fsWin.ShowDialog();
+
+            SelectImage(stack.Children[fsWin.Id]);
+        }
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+        // драг-н-дроп
+        private void MainGrid_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] files = (string[])(e.Data.GetData(DataFormats.FileDrop, true));
+            }
+        }
+        private void MainGrid_PreviewDrop(object sender, DragEventArgs e)
+        {
+            string[] pathes = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+
+            foreach (string path in pathes)
+            {
+                if (Directory.Exists(path))
+                    ExtractFilesFromDir(path);
+
+                else if (File.Exists(path))
+                    list.Add(path);
+            }
+            UpdateStack();
+        }
+        private void ExtractFilesFromDir(string path)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                    list.Add(file);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // прокрутка предпросмотра колесом мыши
+        private void scrollViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+          if(e.Delta > 0) 
+            scrollViewer.LineRight();
+          else if(e.Delta < 0)
+            scrollViewer.LineLeft();
+
+            e.Handled = false;
+        }
+
+        // выделение фото(размер) предпросмотра при наведении мыши
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+                border.Margin = new Thickness(10, 15, 10, 15);
+        }
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+                border.Margin = new Thickness(10, 3, 10, 3);
+        }
+    }
+}
+
