c8b1740eeb6e7024840bc2bcf6262aa5958b0832
Taras Shevchuk <stg11077@gmail.com>
2023-05-05 23:18:30 +0300

    add binding Images, Background-Blur

--- Simple_galery/MainWindow.xaml.cs
+++ Simple_galery/MainWindow.xaml.cs
@@ -28,10 +28,19 @@ namespace Simple_galery
         private System.Windows.Forms.FolderBrowserDialog fbd;
         private List<string> list = new List<string>();
         private Finder finder;
+        private int lastLoadedIndex = 0;
+        private int extraLoad = 3;
+       
+        public ImageSource ImagePreview
+        {
+            get { return (ImageSource)GetValue(ImagePreviewProperty); }
+            set { SetValue(ImagePreviewProperty, value); }
+        }
+                
+        public static readonly DependencyProperty ImagePreviewProperty =
+            DependencyProperty.Register("ImagePreview", typeof(ImageSource), typeof(MainWindow), new PropertyMetadata(null));
 
-        //private Image imagePreview;
 
-        public Image ImagePreview { get; set; }
 
         public MainWindow()
         {
@@ -39,7 +48,10 @@ namespace Simple_galery
 
             fbd = new System.Windows.Forms.FolderBrowserDialog();
             finder = new Finder();
-            ImagePreview = new Image();
+            //ImagePreview = new Image();
+            DataContext = this;
+
+            ConfigWindow();
 
             string[] filePatterns =
             {
@@ -51,6 +63,12 @@ namespace Simple_galery
             };
 
             finder.FileMasks = filePatterns;
+        }
+
+        private void ConfigWindow()
+        {
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
         private void menuOpen_Click(object sender, RoutedEventArgs e)
@@ -68,28 +86,30 @@ namespace Simple_galery
         }
         private void UpdateStack()
         {
-            list.ForEach(path =>
+            for (int i = lastLoadedIndex; i < lastLoadedIndex + extraLoad && i < list.Count; i++)
             {
-                System.Windows.Controls.Image image = new System.Windows.Controls.Image();
-                image.Source = new BitmapImage(new Uri(path));
-                image.MouseWheel += scrollViewer_MouseWheel;
+                    System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                    image.Source = new BitmapImage(new Uri(list[i]));
+                    image.MouseWheel += ScrollViewer_MouseWheel;
 
-                Border border = new Border()
-                {
-                    Child = image,
+                    Border border = new Border()
+                    {
+                        Child = image,
 
-                    MinWidth = 150,
-                    BorderThickness = new Thickness(3, 3, 3, 3),
-                    Margin = new Thickness(10, 15, 10, 15),
-                };
+                        MinWidth = 150,
+                        BorderThickness = new Thickness(3, 3, 3, 3),
+                        Margin = new Thickness(10, 15, 10, 15),
+                    };
 
-                border.MouseDown += Border_MouseDown;
-                border.MouseEnter += Border_MouseEnter;
-                border.MouseLeave += Border_MouseLeave;
-                border.MouseWheel += scrollViewer_MouseWheel;
+                    border.MouseDown += Border_MouseDown;
+                    border.MouseEnter += Border_MouseEnter;
+                    border.MouseLeave += Border_MouseLeave;
+                    //border.MouseWheel += ScrollViewer_MouseWheel;
 
-                stack.Children.Add(border);
-            });
+                    stack.Children.Add(border);
+            };
+
+            lastLoadedIndex += extraLoad;
         }
         private void SelectImage(object sender)
         {
@@ -100,10 +120,16 @@ namespace Simple_galery
                     activeBorder.BorderBrush = new SolidColorBrush(Colors.Transparent);
 
                 activeBorder = border;
-                border.BorderBrush = new SolidColorBrush(Colors.DarkOliveGreen);
+                border.BorderBrush = new SolidColorBrush(Colors.ForestGreen);
 
                 if (border.Child is Image image)
-                    imgPreview.Source = image.Source;
+                    ImagePreview = image.Source;
+
+                if ((bool)rbtnPreviewImage.IsChecked) // установка SelectImage фоном главного изображения
+                    panelPreview.Background = new ImageBrush(ImagePreview);
+
+                if (stack.Children.IndexOf(activeBorder) == lastLoadedIndex-1) // запуск подгрузки, если SelectImage достигло последнего загруженного фото
+                    UpdateStack();
             }
         }
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -166,17 +192,17 @@ namespace Simple_galery
         }
 
         // прокрутка предпросмотра колесом мыши
-        private void scrollViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+        private void ScrollViewer_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-          if(e.Delta > 0) 
-            scrollViewer.LineRight();
-          else if(e.Delta < 0)
-            scrollViewer.LineLeft();
+            if (e.Delta < 0)
+                scrollViewer.LineRight();
+            else if (e.Delta > 0)
+                scrollViewer.LineLeft();
 
             e.Handled = false;
         }
 
-        // выделение фото(размер) предпросмотра при наведении мыши
+        // выделение фото предпросмотра(размер) при наведении мыши
         private void Border_MouseLeave(object sender, MouseEventArgs e)
         {
             if (sender is Border border)
@@ -187,6 +213,20 @@ namespace Simple_galery
             if (sender is Border border)
                 border.Margin = new Thickness(10, 3, 10, 3);
         }
+
+        // обработчики radioButtons изменения фона
+        private void rbtnPreviewImage_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ImagePreview != null)
+                panelPreview.Background = new ImageBrush(ImagePreview);
+        }
+        private void rbtnNone_Checked(object sender, RoutedEventArgs e)
+        {
+            if (panelPreview !=null && panelPreview.Background is ImageBrush)
+                panelPreview.Background = null;
+        }
+
+        
     }
 }
 
