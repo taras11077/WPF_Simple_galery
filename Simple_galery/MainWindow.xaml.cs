e17044ae598d7eaae062c8fd0a9afa8b4e036072
Taras Shevchuk <stg11077@gmail.com>
2023-05-07 21:20:03 +0300

    add Sepia & Delete

--- Simple_galery/MainWindow.xaml.cs
+++ Simple_galery/MainWindow.xaml.cs
@@ -3,19 +3,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using DataFormats = System.Windows.DataFormats;
+using DragEventArgs = System.Windows.DragEventArgs;
+using Image = System.Windows.Controls.Image;
 
 namespace Simple_galery
 {
@@ -31,7 +39,7 @@ namespace Simple_galery
         private int lastLoadedIndex = 0;
         private int extraLoad = 3;
        
-        public ImageSource ImagePreview
+        public ImageSource? ImagePreview
         {
             get { return (ImageSource)GetValue(ImagePreviewProperty); }
             set { SetValue(ImagePreviewProperty, value); }
@@ -46,7 +54,6 @@ namespace Simple_galery
 
             fbd = new System.Windows.Forms.FolderBrowserDialog();
             finder = new Finder();
-            //ImagePreview = new Image();
             DataContext = this;
 
             ConfigWindow();
@@ -86,7 +93,7 @@ namespace Simple_galery
         {
             for (int i = lastLoadedIndex; i < lastLoadedIndex + extraLoad && i < list.Count; i++) // загрузка заданного количества изображений с фиксацией индекса последнешо загруженного
             {
-                    System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+                    Image image = new Image();
                     image.Source = new BitmapImage(new Uri(list[i]));
                     image.MouseWheel += ScrollViewer_MouseWheel;
 
@@ -122,7 +129,7 @@ namespace Simple_galery
 
                 if (border.Child is Image image)
                     ImagePreview = image.Source;
-
+                    
                 if ((bool)rbtnPreviewImage.IsChecked!) // установка SelectImage фоном главного изображения
                     panelPreview.Background = new ImageBrush(ImagePreview);
 
@@ -185,7 +192,7 @@ namespace Simple_galery
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                System.Windows.MessageBox.Show(ex.Message);
             }
         }
 
@@ -201,12 +208,12 @@ namespace Simple_galery
         }
 
         // выделение фото предпросмотра(размер) при наведении мыши
-        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        private void Border_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
             if (sender is Border border)
                 border.Margin = new Thickness(10, 15, 10, 15);
         }
-        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        private void Border_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             if (sender is Border border)
                 border.Margin = new Thickness(10, 3, 10, 3);
@@ -224,7 +231,58 @@ namespace Simple_galery
                 panelPreview.Background = null;
         }
 
-        
+        private void btnRotate_Click(object sender, RoutedEventArgs e)
+        {
+            if (ImagePreview != null)
+            {
+                //Image image = new Image();
+                //image.Source = ImagePreview;
+
+                //RotateTransform rotate = new RotateTransform(90);
+
+                //image.RenderTransform = rotate;
+                //ImagePreview = image.Source;
+                //UpdateStack();
+
+                Image image = new Image();
+                image.Source = ImagePreview;
+
+                var RotateTransform = image.RenderTransform as RotateTransform;
+                var transform = new RotateTransform(1 + (RotateTransform?.Angle ?? 90));
+                image.RenderTransform = transform;
+                ImagePreview = image.Source;
+
+            }
+        }
+
+        private void btnSepia_Click(object sender, RoutedEventArgs e)
+        {
+            if (ImagePreview != null)
+            {
+                FormatConvertedBitmap newFormatedBitmapSource = new FormatConvertedBitmap();
+
+                newFormatedBitmapSource.BeginInit();
+                newFormatedBitmapSource.Source = ImagePreview as BitmapSource;
+                newFormatedBitmapSource.DestinationFormat = PixelFormats.Gray32Float;
+                newFormatedBitmapSource.EndInit();
+
+                Image image = new Image();
+                image.Width = 200;
+                image.Source = newFormatedBitmapSource;
+
+                ImagePreview = image.Source;
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeBorder != null)
+            {
+                ImagePreview = null;
+                list.RemoveAt(stack.Children.IndexOf(activeBorder));
+                stack.Children.Remove(activeBorder);
+            }
+        }
     }
 }
 
